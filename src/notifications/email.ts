ec446b00ac505a2345125733be209b5330c62c65
Adham Salama <adham.salama@wuilt.com>
2023-05-27 17:30:41 +0300

    Replace nodemailer with sendGrid

--- src/notifications/email.ts
+++ src/notifications/email.ts
@@ -1,41 +1,29 @@
-import nodemailer from "nodemailer";
 import { AbstractNotification } from "./types";
 import { usersService } from "../users/service";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 export class EmailNotification implements AbstractNotification {
   async send(to: string, content: string, subject?: string) {
-    console.log("alooooo");
-
-    let testAccount = await nodemailer.createTestAccount();
-    console.log({ testAccount });
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      logger: true,
-      debug: true,
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-      },
-    });
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: testAccount.user, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text: content, // plain text body
-      // html: "<b>Hello world?</b>", // html body
-    });
-    console.log({ info });
-
-    // Preview only available when sending through an Ethereal account
-    console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-    return true;
+    const msg = {
+      to,
+      from: process.env.SENDER_EMAIL!,
+      subject: subject ?? "Hello from our monitoring app",
+      text: content,
+    };
+    const sentSuccessfully = sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+        return response[0].statusCode < 300;
+      })
+      .catch((error) => {
+        console.error(error);
+        return false;
+      });
+    return sentSuccessfully;
   }
   async sendToUser(userId: string, content: string, subject?: string) {
     const user = await usersService.findById(userId);
