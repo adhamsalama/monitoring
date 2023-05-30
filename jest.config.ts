99a7f803a9331c6221495966c683b9cf90f1dd88
Adham Salama <adham.salama@wuilt.com>
2023-05-30 13:34:22 +0300

    Remove comments

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,3 @@
-// import * as dotenv from "dotenv";
-// import { MongoMemoryServer } from "mongodb-memory-server";
-// import mongoose from "mongoose";
-// // import { app } from "./src/index";
-// dotenv.config();
-// beforeAll(async () => {
-//   console.log("aaaaaaa");
-
-//   const mongod = await MongoMemoryServer.create();
-//   const uri = mongod.getUri();
-//   await mongoose
-//     .connect(uri)
-//     .then(() => {
-//       console.log("Connected");
-//     })
-//     .catch((err) => console.log(`error ${err}`));
-//   // app.listen(3000, () => console.log("Testing..."));
-// });
-
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
